e70b6b0e29cd625d21ea3988a4729aa2c94ec355
Jamie Lynn Noon <jamielynnnoon@gmail.com>
2017-10-28 15:42:46 -0700

    updated site

---
+++ source/javascripts/site.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  $(".toggle-nav").click(function(e) {
+    $(this).toggleClass("active");
+    $(".menu > ul").toggleClass("active");
+
+    e.preventDefault();
+  });
+
+});
